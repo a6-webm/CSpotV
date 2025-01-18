827a7da389f9fbe2a6af0716bc671967f99c7f81
a6.webm <46289515+a6-webm@users.noreply.github.com>
2025-01-18 23:31:33 +0000

    get spotify auth code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{error::Error, path::PathBuf};
+use std::{
+    error::Error,
+    fs,
+    io::{self, Write},
+    path::PathBuf,
+};
 
 use clap::{command, Parser, Subcommand};
 use serde::{de::DeserializeOwned, Deserialize};
+use spotify_rs::{AuthCodeClient, AuthCodeFlow, RedirectUrl};
 
 // TODO does the following matter
 //     By default, struct field names are deserialized based on the position of
@@ -57,27 +63,53 @@ fn collect_csv<T: DeserializeOwned>(path: PathBuf) -> Result<Vec<T>, csv::Error>
         .collect::<Result<Vec<T>, csv::Error>>()
 }
 
-fn map(lib_path: PathBuf, map_path: PathBuf) -> Result<(), Box<dyn Error>> {
+async fn map(lib_path: PathBuf, map_path: PathBuf) -> Result<(), Box<dyn Error>> {
     let lib: Vec<LibRecord> = collect_csv(lib_path)?;
     let map: Vec<MapRecord> = collect_csv(map_path)?;
     todo!();
 }
 
-fn check(map_path: PathBuf) -> Result<(), Box<dyn Error>> {
+async fn check(map_path: PathBuf) -> Result<(), Box<dyn Error>> {
     let map: Vec<MapRecord> = collect_csv(map_path)?;
     todo!();
 }
 
-fn upload(map_path: PathBuf) -> Result<(), Box<dyn Error>> {
+async fn upload(map_path: PathBuf) -> Result<(), Box<dyn Error>> {
     let map: Vec<MapRecord> = collect_csv(map_path)?;
     todo!();
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+
+    let redirect_url = RedirectUrl::new("http://localhost/".to_owned())?;
+    let auto_refresh = true;
+    let scopes = vec!["playlist-read-private", "playlist-modify-private"];
+    let auth_code_flow = AuthCodeFlow::new(
+        "fed3e6de8e3e4fe481b4020cdb72342e",
+        fs::read_to_string("client_secret.txt")?,
+        scopes,
+    );
+
+    let (auth_client, url) = AuthCodeClient::new(auth_code_flow, redirect_url, auto_refresh);
+    println!("Enter the following url into a browser:\n\n\t{}\n", url);
+
+    // TODO add option to store the auth stuff and only refresh it if its invalid
+    print!("Then paste the resuting localhost url here: ");
+    io::stdout().flush()?;
+    let mut auth_url = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut auth_url)?;
+    let split: Vec<&str> = auth_url.trim().split(['=', '&']).collect();
+    let auth_code = split[1].to_owned();
+    let csrf_token = split[3].to_owned();
+
+    let mut auth_sp = auth_client.authenticate(auth_code, csrf_token).await?;
+
     match cli.command {
-        Commands::Map { lib_path, map_path } => map(lib_path, map_path),
-        Commands::Check { map_path } => check(map_path),
-        Commands::Upload { map_path } => upload(map_path),
+        Commands::Map { lib_path, map_path } => map(lib_path, map_path).await,
+        Commands::Check { map_path } => check(map_path).await,
+        Commands::Upload { map_path } => upload(map_path).await,
     }
 }
